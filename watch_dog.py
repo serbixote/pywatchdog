17c5a4d1cd1aaec2757f20f2d8f3c1569966f44e
Marco Davalos <marcodavalosmantilla@gmail.com>
2017-06-04 06:13:22 -0700

    readline handler by a thread ++

--- watch_dog.py
+++ watch_dog.py
@@ -1,3 +1,5 @@
+from threading import Thread
+from multiprocessing import Queue
 from subprocess import Popen, PIPE
 from multiprocessing import Process, Manager
 import os
@@ -46,10 +48,25 @@ class FileSystemWatchDog:
                 raise ValueError("Invalid dams, please check the paths")
 
         def __watch_dog_handler(dams, watched_dams, running):
-
             # processes
             dam_processes = {}
 
+            def __compose_command(dam_path):
+                return [INOTIFY, MONITOR, RECURSIVE, EVENT, CREATE, EVENT, MODIFY, EVENT, MOVE_FROM_AND_TO,
+                        EVENT, DELETE, dam_path]
+
+            dam_process = Popen(__compose_command(dams[0]), stdin=PIPE, stdout=PIPE, stderr=PIPE)
+            nbsr = NonBlockingStreamReader(dam_process.stdout)
+
+            while running:
+                output = nbsr.read_line(0.1)
+                print('checking')
+                if output:
+                    print(output)
+                # 0.1 secs to let the shell output the result
+
+            dam_process.kill()
+            '''
             # Compose the command list
             def __compose_command(dam_path):
                 return [INOTIFY, MONITOR, RECURSIVE, EVENT, CREATE, EVENT, MODIFY, EVENT, MOVE_FROM_AND_TO,
@@ -65,16 +82,14 @@ class FileSystemWatchDog:
                 for dam_process in dam_processes:
                     if dam_processes[dam_process].poll() is None:
                         pass
-                        '''
                         event = dam_processes[dam_process].stdout.readline().decode('utf-8')
                         if event is not None:
                             watched_dams[dam_process] = event
-                        '''
-
             # Stopping subprocess
             for dam_process in dam_processes:
                 if dam_processes[dam_process].poll() is None:
                     dam_processes[dam_process].kill()
+        '''
 
         self.process = Process(target=__watch_dog_handler, args=(self.dams, self.watched_dams, self.watching))
         self.process.start()
@@ -84,5 +99,48 @@ class FileSystemWatchDog:
 
     def hold_on_to_the_watch_dog(self):
         self.watching.value = False
+
+
+class NonBlockingStreamReader:
+
+    def __init__(self, stream):
+
+        self._s = stream
+        self._q = Queue()
+
+        def _populate_queue(stream, queue):
+
+            while True:
+                line = stream.readline()
+                if line:
+                    queue.put(line)
+                else:
+                    raise UnexpectedEndOfStream
+
+        self._t = Thread(target=_populate_queue,  args=(self._s, self._q))
+        self._t.daemon = True
+        self._t.start()
+
+    def read_line(self, timeout=None):
+        try:
+            return self._q.get(block=timeout is not None, timeout=timeout)
+        except Exception:
+            return None
+
+
+class UnexpectedEndOfStream(Exception):
+    pass
+
+
+def init():
+
+    watchdog = FileSystemWatchDog(['/home/developer/Documents/test_01'])
+    watchdog.release_the_watch_dog()
+    input('tocontinue')
+    print(watchdog.get_caught_dams())
+    print(watchdog.hold_on_to_the_watch_dog())
+
+if __name__ == '__main__':
+    init()
 
 
