15d2b85ee6c94f86500db4c5736ecf92b86c0e1f
Marco Davalos <marcodavalosmantilla@gmail.com>
2017-06-04 18:39:01 +0200

    release_the_watch_dog method was changed

--- watch_dog.py
+++ watch_dog.py
@@ -1,5 +1,4 @@
-from threading import Thread
-from multiprocessing import Queue
+from _signal import SIGKILL
 from subprocess import Popen, PIPE
 from multiprocessing import Process, Manager
 import os
@@ -26,8 +25,8 @@ class FileSystemWatchDog:
         self.dams = dams
         self.process = None
         self.manager = Manager()
-        self.watching = self.manager.Value('running', True)
-        self.watched_dams = self.manager.dict()
+        self.subprocess_pid = self.manager.Value('pid', -1)
+        self.caught_dams = self.manager.dict()
 
     @staticmethod
     def are_valid_dams(dams):
@@ -47,100 +46,37 @@ class FileSystemWatchDog:
             else:
                 raise ValueError("Invalid dams, please check the paths")
 
-        def __watch_dog_handler(dams, watched_dams, running):
-            # processes
-            dam_processes = {}
+        def __watch_dog_handler(dams, caught_dams, subprocess_pid):
 
+            # compose a command to call the bash script TODO: creating bash script and modify __compose_command
             def __compose_command(dam_path):
                 return [INOTIFY, MONITOR, RECURSIVE, EVENT, CREATE, EVENT, MODIFY, EVENT, MOVE_FROM_AND_TO,
                         EVENT, DELETE, dam_path]
 
-            dam_process = Popen(__compose_command(dams[0]), stdin=PIPE, stdout=PIPE, stderr=PIPE)
-            nbsr = NonBlockingStreamReader(dam_process.stdout)
+            # subprocess starts and its pid is saved
+            dam_process = Popen(__compose_command(dams[0]), stdin=PIPE, stdout=PIPE, stderr=PIPE, preexec_fn=os.setsid)
+            subprocess_pid.value = dam_process.pid
 
-            while running:
-                output = nbsr.read_line(0.1)
-                print('checking')
-                if output:
-                    print(output)
-                # 0.1 secs to let the shell output the result
+            # loop checks subprocess' output
+            while dam_process.poll() is None:
 
-            dam_process.kill()
-            '''
-            # Compose the command list
-            def __compose_command(dam_path):
-                return [INOTIFY, MONITOR, RECURSIVE, EVENT, CREATE, EVENT, MODIFY, EVENT, MOVE_FROM_AND_TO,
-                        EVENT, DELETE, dam_path]
+                try:
+                    output = dam_process.stdout.readline().decode('utf-8')
+                    if output:
+                        # TODO: parse result method
+                        caught_dams['event'] = output
+                except Exception:
+                    break
 
-            # Starting subprocess
-            for dam in dams:
-                dam_process = Popen(__compose_command(dam), stdin=PIPE, stdout=PIPE, stderr=PIPE)
-                dam_processes[dam] = dam_process
-
-            # Watching dams
-            while running.value:
-                for dam_process in dam_processes:
-                    if dam_processes[dam_process].poll() is None:
-                        pass
-                        event = dam_processes[dam_process].stdout.readline().decode('utf-8')
-                        if event is not None:
-                            watched_dams[dam_process] = event
-            # Stopping subprocess
-            for dam_process in dam_processes:
-                if dam_processes[dam_process].poll() is None:
-                    dam_processes[dam_process].kill()
-        '''
-
-        self.process = Process(target=__watch_dog_handler, args=(self.dams, self.watched_dams, self.watching))
+        # process starts __watch_dog_handler
+        self.process = Process(target=__watch_dog_handler, args=(self.dams, self.caught_dams, self.subprocess_pid))
         self.process.start()
 
     def get_caught_dams(self):
-        return self.watched_dams
+        return self.caught_dams
 
     def hold_on_to_the_watch_dog(self):
-        self.watching.value = False
-
-
-class NonBlockingStreamReader:
-
-    def __init__(self, stream):
-
-        self._s = stream
-        self._q = Queue()
-
-        def _populate_queue(stream, queue):
-
-            while True:
-                line = stream.readline()
-                if line:
-                    queue.put(line)
-                else:
-                    raise UnexpectedEndOfStream
-
-        self._t = Thread(target=_populate_queue,  args=(self._s, self._q))
-        self._t.daemon = True
-        self._t.start()
-
-    def read_line(self, timeout=None):
-        try:
-            return self._q.get(block=timeout is not None, timeout=timeout)
-        except Exception:
-            return None
-
-
-class UnexpectedEndOfStream(Exception):
-    pass
-
-
-def init():
-
-    watchdog = FileSystemWatchDog(['/home/developer/Documents/test_01'])
-    watchdog.release_the_watch_dog()
-    input('tocontinue')
-    print(watchdog.get_caught_dams())
-    print(watchdog.hold_on_to_the_watch_dog())
-
-if __name__ == '__main__':
-    init()
-
-
+        if self.subprocess_pid.value != -1:
+            os.killpg(os.getpgid(self.subprocess_pid.value), SIGKILL)
+        if self.process.is_alive():
+            self.process.terminate()
